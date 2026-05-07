2003f7fbef0aa19fea0a1ad60ac8ae2384095544
Lucas Nogueira <lucas@tauri.app>
2026-05-07 09:22:46 -0300

    build

--- download-cef/src/lib.rs
+++ download-cef/src/lib.rs
@@ -540,7 +540,7 @@ where
         cef_dir.join(LIBCEF_DLL_DIR),
     )?;
     const CREDITS_HTML: &str = "CREDITS.html";
-    fs::rename(extracted_dir.join(CREDITS_HTML), cef_dir.join(CREDITS_HTML))?;
+    fs_err::rename(extracted_dir.join(CREDITS_HTML), cef_dir.join(CREDITS_HTML))?;
 
     if show_progress {
         println!("Moved contents to: {}", cef_dir.display());
